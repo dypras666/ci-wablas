a10562166217a60a2d46335a844e7202567124dc
Kurniawan <project@lampungtech.com>
2020-03-20 17:54:50 +0700

    Update Wablas.php

--- Wablas.php
+++ Wablas.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed'); 
-class Whatsapp extends MY_Controller 
+class Wablas extends MY_Controller 
 {
     public function __construct()
     {
